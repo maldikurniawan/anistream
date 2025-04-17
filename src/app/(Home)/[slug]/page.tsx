07343cd0bdaef9dd6ea8b4b9ee1f6d764b30264c
maldikurniawan <akurniawan2102@gmail.com>
2025-04-17 09:28:21 +0700

    fix slug

--- src/app/(Home)/[slug]/page.tsx
+++ src/app/(Home)/[slug]/page.tsx
@@ -1,22 +1,23 @@
 import { Footer, Header } from '@/components';
-import React from 'react'
+import React from 'react';
 
-interface AnimeDetailPageProps {
-    params: {
-        slug: string;
-    };
+interface PageProps {
+  params: {
+    slug: string;
+  };
 }
 
-const AnimeDetailPage = ({ params }: AnimeDetailPageProps) => {
-    return (
-        <main>
-            <Header />
-            <div className="min-h-screen bg-[#1F1F1F] text-white pt-[94px] p-4 sm:pt-[130px] sm:px-[60px] sm:pb-8">
-                <p>Slug: {params.slug}</p>
-            </div>
-            <Footer />
-        </main>
-    );
+const AnimeDetailPage = ({ params }: PageProps) => {
+  return (
+    <main>
+      <Header />
+      <div className="min-h-screen bg-[#1F1F1F] text-white pt-[94px] p-4 sm:pt-[130px] sm:px-[60px] sm:pb-8">
+        <h1 className="text-2xl font-bold mb-4">Detail Anime</h1>
+        <p>Slug: <span className="text-[#E3BFFF]">{params.slug}</span></p>
+      </div>
+      <Footer />
+    </main>
+  );
 };
 
 export default AnimeDetailPage;
